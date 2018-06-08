f7c0382fb91ab434af6705dbdb7e48554c3161e3
scott coulton <scottc@puppet.com>
2018-06-08 11:13:34 +1000

    moved timeout to 5 seconds

--- bootstrap/main.go
+++ bootstrap/main.go
@@ -11,7 +11,7 @@ func main() {
 
 	for true {
 		// Time to wait for the kubelet container to start
-		time.Sleep(30 * time.Second)
+		time.Sleep(5 * time.Second)
 		run("services.linuxkit", nextExecID(), "kubelet", []string{"kubeadm-init.sh"})
 
 	}
