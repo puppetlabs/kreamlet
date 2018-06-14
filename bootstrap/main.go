d479b988f741ed3f16db03b154089adaf1ec7518
Mark Fuller <mark.fuller@puppet.com>
2018-06-14 20:22:00 +0100

    Basic gRPC serving plus stub client

--- bootstrap/main.go
+++ bootstrap/main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"bufio"
+	"context"
 	"fmt"
+	"io/ioutil"
 	"log"
+	"net"
 	"regexp"
 	"strings"
 	"time"
@@ -11,17 +14,44 @@ import (
 	"crypto/rand"
 
 	"github.com/puppetlabs/kreamlet/bootstrap/kubelet"
+	pb "github.com/puppetlabs/kreamlet/bootstrap/messaging"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+const (
+	port                    = ":50091"
+	adminCredsFromLocalFile = true
+	pathToCredsFile         = "/etc/kubernetes/admin.conf"
 )
 
 func main() {
-	taskRoot := random()
-	var output string
-	var err error
+	//in a background go routine start a tcp listener for grpc connections
+	go startListening()
 
 	// Time to wait for the kubelet container to start
 	time.Sleep(5 * time.Second)
 
-	// Initalise the cluster
+	taskRoot := random()
+
+	err := initKubeAdm(taskRoot)
+	if err != nil {
+		//if initialising kube admin doesn't succeed, there is nothing we can do here, so just exit
+		log.Fatal(err)
+	}
+
+	//for now keep the main thread alive whilst we wait for a tcp connection
+	//(we should be make a listener channel and waiting for it to complete?)
+	for true {
+		fmt.Println("Listening.....")
+		time.Sleep(time.Minute)
+	}
+}
+
+func initKubeAdm(taskRoot string) error {
+	var output string
+	var err error
+
 	output, err = kubelet.Run("services.linuxkit", nextExecID(taskRoot), "kubelet", []string{"kubeadm-init.sh"})
 	if err != nil {
 		log.Fatal(err)
@@ -29,13 +59,63 @@ func main() {
 	fmt.Printf("output is \n%v\n", output)
 	joinToken, err := getJoinToken(output)
 	fmt.Printf("join token is \n%v\n", joinToken)
+	return err
 
-	// Get the admin creds
-	output, err = kubelet.Run("services.linuxkit", nextExecID(taskRoot), "kubelet", []string{"cat", "/etc/kubernetes/admin.conf"})
-	if err != nil {
-		log.Fatal(err)
+}
+
+// server is used to implement helloworld.GreeterServer.
+type server struct{}
+
+// GetAdminCreds implements AdminCredsServer.GetAdminCreds
+func (s *server) GetAdminCreds(ctx context.Context, in *pb.AdminCredsRequest) (*pb.AdminCredsResponse, error) {
+	r := &pb.AdminCredsResponse{}
+
+	var content []byte
+	var err error
+
+	if adminCredsFromLocalFile {
+		content, err = getAdminCredsFromLocalFile()
+	} else {
+		content, err = getAdminCredsViaContainerd()
+
 	}
-	fmt.Printf("output is \n%v\n", output)
+	if err == nil {
+		r.Content = content
+		r.StatusCode = pb.StatusCode_Ok
+	} else {
+		r.StatusCode = pb.StatusCode_Failed
+		r.Message = err.Error()
+	}
+	fmt.Printf("Returning %v and err of %v\n", r, err)
+	return r, err
+}
+
+func getAdminCredsViaContainerd() ([]byte, error) {
+	var output string
+	var err error
+	output, err = kubelet.Run("services.linuxkit", nextExecID(random()), "kubelet", []string{"cat", "/etc/kubernetes/admin.conf"})
+	fmt.Printf("output is \n%v and err is %v\n", output, err)
+	return []byte(output), err
+}
+
+func getAdminCredsFromLocalFile() ([]byte, error) {
+	return ioutil.ReadFile(pathToCredsFile)
+}
+
+func startListening() {
+	fmt.Printf("Entered startListening about to listen on port %v\n", port)
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	pb.RegisterAdminCredsServer(s, &server{})
+	reflection.Register(s)
+	fmt.Printf("About to listen on port %v\n", port)
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+	fmt.Printf("Listening on port %v\n", port)
 }
 
 func random() string {
---
+++ bootstrap/stubclient/main.go
@@ -0,0 +1,33 @@
+//Package main (stubclient) calls the grpc endpoint GetAdminCreds over tcp
+package main
+
+import (
+	"log"
+	"time"
+
+	pb "github.com/puppetlabs/kreamlet/bootstrap/messaging"
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+)
+
+const (
+	address = "localhost:50091"
+)
+
+func main() {
+	// Set up a connection to the server.
+	conn, err := grpc.Dial(address, grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+	c := pb.NewAdminCredsClient(conn)
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	r, err := c.GetAdminCreds(ctx, &pb.AdminCredsRequest{})
+	if err != nil {
+		log.Fatalf("could not invoke admin creds server: %v", err)
+	}
+	log.Printf("Response: %s", r)
+}
