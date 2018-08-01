a8f157c89dd1c0654b7c1dd8e9ca8e43b1a86792
scott coulton <scottc@puppet.com>
2018-08-01 13:19:14 +1000

    adds ssh terminal and cli command

--- cmd/ssh.go
+++ cmd/ssh.go
@@ -1,9 +1,13 @@
 package cmd
 
 import (
+	"bufio"
 	"fmt"
-	//"github.com/puppetlabs/kreamlet/client"
+	"github.com/puppetlabs/kreamlet/client"
 	"github.com/spf13/cobra"
+	"golang.org/x/crypto/ssh"
+	"os"
+	"strings"
 )
 
 var SshCmd = &cobra.Command{
@@ -22,6 +26,44 @@ func init() {
 }
 
 func SshController() {
-	fmt.Println("comming soon")
-	return
+
+	client, session, err := client.Ssh("root", "127.0.0.1:2222")
+	if err != nil {
+		fmt.Println("could not connect to the node. please make sure it is running")
+	}
+	defer client.Close()
+	// Set redirection of stdout and stderr
+	session.Stdout = os.Stdout
+	session.Stderr = os.Stderr
+	in, _ := session.StdinPipe()
+	// Set up terminal
+	modes := ssh.TerminalModes{
+		ssh.ECHO:          0,     // disable echoing
+		ssh.TTY_OP_ISPEED: 14400, // input speed = 14.4kbaud
+		ssh.TTY_OP_OSPEED: 14400, // output speed = 14.4kbaud
+	}
+
+	// Request pseudo terminal
+	if err := session.RequestPty("xterm", 80, 40, modes); err != nil {
+		fmt.Println("request for pseudo terminal failed: %s", err)
+		os.Exit(1)
+	}
+	// Start remote shell
+	if err := session.Shell(); err != nil {
+		fmt.Println("failed to start shell: %s", err)
+		os.Exit(1)
+	}
+	// Accepting commands
+	for {
+		reader := bufio.NewReader(os.Stdin)
+		str, _ := reader.ReadString('\n')
+		fmt.Fprint(in, str)
+	}
+}
+
+func scanConfig() string {
+	config, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+	config = strings.Trim(config, "\n")
+	return config
+
 }
